4f60002932c24114fe48edf52ddc3dc925b43ef6
noashimada <n.shimada@genbasupport.com>
2026-07-28 14:18:00 +0900

    01.fizzbuzz配下にfizzbuzz.rbを追加

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+x = (1..20).to_a 
+
+x.each do |x|
+  case
+  when x % 15 == 0
+    puts "Fizz" 
+  when x % 3 == 0
+      puts "Buzz"
+  when x % 5 == 0
+      puts "FizzBuzz"
+  else puts x.to_s
+  end
+end
