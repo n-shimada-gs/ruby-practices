5c9b3fc82db93669eb12070a3cc0ff2d15aafe53
noashimada <n.shimada@genbasupport.com>
2026-07-28 14:56:18 +0900

    fizzbuzzのコードを修正_四回目の提出

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -3,11 +3,11 @@ x = (1..20).to_a
 x.each do |x|
   case
   when x % 15 == 0
-    puts "Fizz" 
+    puts "FizzBuzz" 
   when x % 3 == 0
-    puts "Buzz"
+    puts "Fizz"
   when x % 5 == 0
-    puts "FizzBuzz"
+    puts "Buzz"
   else puts x.to_s
   end
 end
