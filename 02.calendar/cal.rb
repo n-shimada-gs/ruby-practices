698670cd9ec5531878cfe0ef99eb4f43990c31b2
noashimada <n.shimada@genbasupport.com>
2026-07-29 15:13:54 +0900

    ファイル末尾に改行を追加_二回目

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,7 +5,7 @@ require 'optparse'
 
 # デフォルト(引数を指定しない場合は、今月・今年のカレンダーが表示)
 options = {
-  m: Date.today.month,  # 間にスペースを追加
+  m: Date.today.month,
   y: Date.today.year
 }
 # 引数設定
@@ -14,12 +14,12 @@ opt.on('-y year', Integer) {|v| options[:y] = v }
 opt.on('-m month', Integer) {|v| options[:m] = v }
 opt.parse!(ARGV)
 
-first_day = Date.new(options[:y], options[:m], 1)  # today = Date.todayを削除
+first_day = Date.new(options[:y], options[:m], 1)
 first_wday = first_day.wday
 last_day = Date.new(options[:y], options[:m], -1)
 
 # ヘッダー
-puts "#{options[:m]}月 #{options[:y]}".center(20)  # ヘッダーが中央にくるように指定
+puts "#{options[:m]}月 #{options[:y]}".center(20)
 puts "日 月 火 水 木 金 土"
 
 # 1日までの空白
@@ -29,7 +29,7 @@ print "   " * first_wday
 (first_day..last_day).each do |date|
   print date.day.to_s.rjust(2) + " "
 
-  if date.saturday?  # インデント修正
+  if date.saturday?
     puts ""
   end
 end
