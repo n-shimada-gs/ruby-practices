b7269d88460549217c8c58cb3cad4dc68f30eecd
noashimada <n.shimada@genbasupport.com>
2026-07-29 14:47:34 +0900

    1_today = Date.todayを削除 2_インデント修正 3_m:y:前後にスペース追加 4_ファイル末尾に改行追加 5_カレンダーヘッダーの中央揃え

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,8 +5,8 @@ require 'optparse'
 
 # デフォルト(引数を指定しない場合は、今月・今年のカレンダーが表示)
 options = {
-    m:Date.today.month,
-    y:Date.today.year
+   m: Date.today.month,  # 間にスペースを追加
+   y: Date.today.year
 }
 # 引数設定
 opt = OptionParser.new
@@ -14,13 +14,12 @@ opt.on('-y year', Integer) {|v| options[:y] = v }
 opt.on('-m month', Integer) {|v| options[:m] = v }
 opt.parse!(ARGV)
 
-today = Date.today
-first_day = Date.new(options[:y], options[:m], 1)
+first_day = Date.new(options[:y], options[:m], 1)  # today = Date.todayを削除
 first_wday = first_day.wday
 last_day = Date.new(options[:y], options[:m], -1)
 
 # ヘッダー
-puts "     #{options[:m]}月 #{options[:y]}"
+puts "#{options[:m]}月 #{options[:y]}".center(20)  # ヘッダーが中央にくるように指定
 puts "日 月 火 水 木 金 土"
 
 # 1日までの空白
@@ -28,12 +27,13 @@ print "   " * first_wday
 
 # 繰り返し日付表示
 (first_day..last_day).each do |date|
-    print date.day.to_s.rjust(2) + " "
+  print date.day.to_s.rjust(2) + " "
 
-    if date.saturday?
-        puts ""
-    end
+  if date.saturday?  # インデント修正
+    puts ""
+  end
 end
 
 # ターミナルのプロンプト巻き込み回避用
 puts ""
+# 改行を追加
