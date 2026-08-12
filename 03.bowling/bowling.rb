bad55381ba344457d0feaf7f331998888e21dc26
noashimada <n.shimada@genbasupport.com>
2026-08-12 17:37:11 +0900

    修正の提出_二回目

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 score = ARGV[0]
-scores = score.split(',') # 投球毎に分割する
+scores = score.split(',')
 shots = scores.map { |s| s == 'X' ? 10 : s.to_i }
 remaining = shots.dup
 
@@ -16,25 +16,27 @@ frames_1to9_shots = []
                        end
 end
 
-frame10_shots = remaining # 残りが10フレーム目の文字列データ
+frame10_shots = remaining
 
 frames = frames_1to9_shots.each_slice(2).to_a << frame10_shots
 
 point = frames.each_with_index.sum do |frame, i|
-  if i == 9
-    frame.sum
-  elsif frame[0] == 10 # strike
-    if i == 8
-      10 + frames[i + 1][0] + frames[i + 1][1]
-    elsif frames[i + 1][0] == 10 # ダブルストライク
-      10 + 10 + frames[i + 2][0]
+  bonus =
+    if i == 9
+      0
+    elsif frame[0] == 10 # strike
+      if i == 8
+        frames[i + 1][0, 2].sum
+      elsif frames[i + 1][0] == 10
+        10 + frames[i + 2][0]
+      else
+        frames[i + 1].sum
+      end
+    elsif frame.sum == 10 # spare
+      frames[i + 1][0]
     else
-      10 + frames[i + 1].sum # 通常のストライクの計算
+      0
     end
-  elsif frame.sum == 10 # spare
-    frame.sum + frames[i + 1][0]
-  else
-    frame.sum
-  end
+  frame.sum + bonus
 end
 puts point
